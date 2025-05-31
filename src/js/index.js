078fa1e922b442551b18356d6aca01ac9ec67ed4
Marcio Santos <santosu01@gmail.com>
2025-05-31 12:26:17 -0300

    feat: add form validation

--- src/js/index.js
+++ src/js/index.js
@@ -3,51 +3,48 @@ function getElementValueById(id) {
   return input.value;
 }
 
-const button = document.querySelector("button");
-button.addEventListener("click", (e) => {
+function setInputError(id, text) {
+  const inputError = document.getElementById(id);
+
+  if (inputError) {
+    inputError.textContent = text;
+  }
+}
+
+function validateInput(elementId, elementErrorId, textError) {
+  const inputValue = getElementValueById(elementId);
+
+  if (inputValue === undefined || inputValue === null || inputValue === "") {
+    setInputError(elementErrorId, textError);
+  }
+}
+
+function validateForm() {
+  const fields = [
+    { fieldName: "name", messageError: "Nome é obrigatório." },
+    { fieldName: "email", messageError: "E-mail é obrigatório." },
+    { fieldName: "phone", messageError: "Telefone é obrigatório." },
+    { fieldName: "password", messageError: "Senha é obrigatório." },
+    {
+      fieldName: "password-confirm",
+      messageError: "A confirmação da senha é obrigatória.",
+    },
+  ];
+
+  for (let i = 0; i < fields.length; i++) {
+    const field = fields[i];
+    const elementErrorId = `${field.fieldName}-error`;
+    validateInput(field.fieldName, elementErrorId, field.messageError);
+  }
+}
+
+const handleClick = (e) => {
   e.preventDefault();
 
-  const name = getElementValueById("name");
-  const email = getElementValueById("email");
-  const phone = getElementValueById("phone");
-  const password = getElementValueById("password");
-  const passwordConfirm = getElementValueById("password-confirm");
+  validateForm();
+};
 
-  console.log(name, email, phone, password, passwordConfirm);
+const button = document.querySelector("button");
+button.addEventListener("click", handleClick);
 
-
-  /*if (name === undefined || name === null || name === "") {
-    console.log("Name está vazio");
-    const nameError = document.getElementById("name-error");
-    console.log(nameError);
-    nameError.textContent = "Nome é obrigatório.";
-  }
-
-  if (email === undefined || email === null || email === "") {
-    console.log("E-mail está vazio");
-    const emailError = document.getElementById("email-error");
-    console.log(emailError);
-    emailError.textContent = "E-mail é obrigatório.";
-  }
-
-  if (phone === undefined || phone === null || phone === "") {
-    console.log("Telefone está vazio");
-    const phoneError = document.getElementById("phone-error");
-    console.log(phoneError);
-    phoneError.textContent = "Telefone é obrigatório.";
-  }
-
-  if (password === undefined || password === null || password === "") {
-    console.log("A senha está vazia");
-    const passwordError = document.getElementById("password-error");
-    console.log(passwordError);
-    passwordError.textContent = "Telefone é obrigatório.";
-  }
-
-  if (passwordConfirm === undefined || passwordConfirm === null || passwordConfirm === "") {
-    console.log("A confirmção da senha está vazia");
-    const passwordConfirmError = document.getElementById("passwordConfirm-error");
-    console.log(passwordConfirmError);
-    passwordConfirmError.textContent = "A confirmação da senha é obrigatória.";
-  }/*
-});
+//Usar abstração usando funções.
